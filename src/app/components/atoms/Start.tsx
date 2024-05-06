f045421312ba99ed30eafdf9dce2d49aae3f28cc
佐藤 朋夏 <127937388+TomkaSato@users.noreply.github.com>
2024-05-06 14:00:45 +0900

    Update Start.tsx

--- src/app/components/atoms/Start.tsx
+++ src/app/components/atoms/Start.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Start = () => {
   return (
     <>
-      <div className="mx-auto w-[80%] md:w-[500px] h-[720px] md:pt-[100px] pt-[100px]">
+      <div className="mx-auto w-[80%] md:w-[500px] md:pt-[100px] pt-[100px]">
         <img className="md:mt-0 mt-[50px] mb-5 mx-auto w-[480px]" src="https://nextjs-sample-9u6c.vercel.app//pokemon-zukan/gengar.png" />
       </div>
     </>
